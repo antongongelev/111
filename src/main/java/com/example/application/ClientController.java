5b19ff22ed026f2e2e75d9f5be99507a0e699688
Anton <antongongelev@gmail.com>
2019-05-06 15:42:45 +0300

    new

--- src/main/java/com/example/application/ClientController.java
+++ src/main/java/com/example/application/ClientController.java
@@ -4,9 +4,7 @@ import com.example.application.domain.Client;
 import com.example.application.repos.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @CrossOrigin
@@ -14,10 +12,18 @@ public class ClientController {
     @Autowired
     private ClientRepo clientRepo;
 
-    @GetMapping("/")
+    @GetMapping("/clients/get-all")
     @ResponseBody
     public Iterable<Client> add() {
         return clientRepo.findAll();
     }
+
+//    @PostMapping("/clients/create")
+//    @ResponseBody
+//    public Iterable<Client> create(@RequestBody (required = false )Client client) {
+//       // clientRepo.save(client);
+//        return clientRepo.findAll();
+//
+//    }
 
 }
