d8b63384ae9ef6ec410c50c275891e70fc3216c8
Anton <antongongelev@gmail.com>
2019-05-27 02:20:20 +0300

    Contracts CRUD

--- src/main/java/com/example/application/domain/Client.java
+++ src/main/java/com/example/application/domain/Client.java
@@ -1,7 +1,5 @@
 package com.example.application.domain;
 
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
